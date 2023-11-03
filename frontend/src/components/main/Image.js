39c01ed65e4e883442df74482b2785dbe0f0b617
Irs444 <124651467+Irs444@users.noreply.github.com>
2023-11-03 21:09:37 +0530

    ...

--- frontend/src/components/main/Image.js
+++ frontend/src/components/main/Image.js
@@ -1,5 +1,14 @@
 
 import React, { useEffect, useState } from 'react';
+import {
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBCardImage,
+    MDBIcon,
+} from "mdb-react-ui-kit";
  
 const Image = () => {
 
@@ -23,24 +32,73 @@ const Image = () => {
 
     return (
         
-  <div >
-{
-    photoList.map((photo) => (
-    <div className="card" style={{ width: "18rem" }}>
-  <img
-    src={'http://localhost:5000/photo.image'}
-    className="card-img-top"
-    alt="Sunset Over the Sea"
-  />
-  <div className="card-body">
-    <p className="card-text">
-     {photo.title}
-    </p>
-  </div>
-</div>
-))}
+        <div>
+        <MDBContainer className="my-5">
+            <MDBRow>
+                {
+                    photoList.map((photo) => (
+                        <MDBCol md="12" lg="4" className="mb-4 mb-lg-5">
+                            <MDBCard style={{ height: "550px", backgroundColor: "#F4F4F4" }}>
+                                <div className="d-flex justify-content-between p-3">
+                                    {/* <p className="lead mb-0">Today's Combo Offer</p> */}
+                                    <div
+                                    // className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong"
+                                    // style={{ width: "35px", height: "35px" }}
+                                    >
+                                        {/* <p className="text-white mb-0 small">x4</p> */}
+                                    </div>
+                                </div>
+                                <MDBCardImage className='img-fluid rounded'
+                                    src={'http://localhost;5000/photo.image'}
+                                    position="top"
+                                    alt="Laptop" style={{ height: "250px" }}
+                                />
+                                <MDBCardBody style={{ height: "350px " }}>
 
-</div>
+
+                                    <div className="d-flex justify-content-between " >
+                                        <h5 className="mb-0" style={{
+                                            lineHeight: "1em",
+                                            height: " 3em",
+                                            overflow: "hidden"
+                                        }}>{photo.title}</h5>
+                                        <h5 className="text-dark mb-0">₹{photo.price}</h5>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <p className="small">
+                                            <a href="#!" className="text-muted">
+                                                {photo.title}
+                                            </a>
+                                        </p>
+                                        <p className="small text-danger">
+                                            {/* <s>{equipment.price}</s> */}
+                                        </p>
+                                    </div>
+
+                                    <div class="d-flex justify-content-between mb-2">
+                                        <p class="text-muted mb-0">
+                                            Available: <span class="fw-bold">6</span>
+                                        </p>
+                                        <div class="ms-auto text-warning">
+                                            <MDBIcon fas icon="star" />
+                                            <MDBIcon fas icon="star" />
+                                            <MDBIcon fas icon="star" />
+                                            <MDBIcon fas icon="star" />
+                                            <MDBIcon fas icon="star" />
+                                        </div>
+                                    </div>
+                                    
+                                    
+                                </MDBCardBody>
+                            </MDBCard>
+                        </MDBCol>
+                    ))
+                }
+
+            </MDBRow>
+        </MDBContainer>
+
+    </div>
 
         
     )
