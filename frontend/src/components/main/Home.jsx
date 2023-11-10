511c727263c9e7b16e019787cd6c5b262c260f8a
Irs444 <124651467+Irs444@users.noreply.github.com>
2023-11-10 16:16:18 +0530

    ....

--- frontend/src/components/main/Home.jsx
+++ frontend/src/components/main/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Home = () => {
     return(
-        <div>
-            <h1>Home Page</h1>
+        <div >
+           <img src='https://th.bing.com/th/id/R.bda158f298039f92632aeb93cc95e9b3?rik=aWv20%2fwy10exvA&riu=http%3a%2f%2fmoblivious.com%2fwp-content%2fuploads%2f2012%2f11%2fInstagram-Web-Profile.png&ehk=rodZljxG025HG4U3sEEGapOTkgKvcX%2fRVEgOCuQFzhI%3d&risl=&pid=ImgRaw&r=0' style={{width:"100%"}}></img>
         </div>
     )
 }
