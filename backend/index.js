04a1a433a5354953f1effad5d37cc29506263a14
Irs444 <124651467+Irs444@users.noreply.github.com>
2023-11-10 12:43:11 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,16 @@ const photoRouter = require('./routers/photoRouter')
 const utilRouter = require('./routers/util')
 
 const cors = require('cors');
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({origin: ['http://localhost:3000', 'https://digi-assignment1.onrender.com']}));
 
 app.use(express.json());
 app.use('/user', userRouter);
 app.use('/photo', photoRouter);
 app.use('/util', utilRouter);
+
+app.get('/', (req, res) => {
+    res.send('backend working');
+})
 
 app.use(express.static('./static/uploads'));
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 const app_config = {
-    API_URL : 'http://localhost:5000'
+    // API_URL : 'http://localhost:5000'
+    API_URL : 'https://backend-nhe8.onrender.com'
 }
 
 export default app_config
